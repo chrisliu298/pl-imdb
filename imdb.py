f3a1376a385f83fecb00fcb09bfdf1f5a32622e2
chrisliu298 <yuhaoliu298@gmail.com>
2020-12-27 14:16:49 -0800

    TODO: fix acc/loss do not change

--- imdb.py
+++ imdb.py
@@ -1,27 +1,11 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.functional as F
 import torch.optim as optim
 from datasets import load_dataset
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
 from torch.utils.data import DataLoader, TensorDataset
-
-
-def dict_to_obj(d):
-    if isinstance(d, list):
-        d = [dict_to_obj(x) for x in d]
-    if not isinstance(d, dict):
-        return d
-
-    class Class:
-        pass
-
-    obj = Class()
-    for k in d:
-        obj.__dict__[k] = dict_to_obj(d[k])
-    return obj
 
 
 def binary_acc(pred, y):
@@ -36,7 +20,7 @@ class DataModule:
         self.config = config
         self.batch_size = config.batch_size
         self.num_embeddings = config.num_embeddings
-        self.seq_len = config.seq_len
+        self.sequence_length = config.sequence_length
 
     def prepare_data(self):
         # Download imdb dataset
@@ -60,19 +44,19 @@ class DataModule:
         # Convert text to integer sequences and pad sequences to the same length
         train_text = pad_sequences(
             tokenizer.texts_to_sequences(train_text),
-            maxlen=self.seq_len,
+            maxlen=self.sequence_length,
             truncating="post",
             padding="post",
         )
         val_text = pad_sequences(
             tokenizer.texts_to_sequences(val_text),
-            maxlen=self.seq_len,
+            maxlen=self.sequence_length,
             truncating="post",
             padding="post",
         )
         test_text = pad_sequences(
             tokenizer.texts_to_sequences(test_text),
-            maxlen=self.seq_len,
+            maxlen=self.sequence_length,
             truncating="post",
             padding="post",
         )
@@ -89,53 +73,83 @@ class DataModule:
 
     def train_dataloader(self):
         return DataLoader(
-            self.train, batch_size=self.batch_size, num_workers=2, pin_memory=True
+            self.train,
+            batch_size=self.batch_size,
+            num_workers=2,
+            pin_memory=True,
+            drop_last=True,
         )
 
     def val_dataloader(self):
         return DataLoader(
-            self.val, batch_size=self.batch_size, num_workers=2, pin_memory=True
+            self.val,
+            batch_size=self.batch_size,
+            num_workers=2,
+            pin_memory=True,
+            drop_last=True,
         )
 
     def test_dataloader(self):
         return DataLoader(
-            self.test, batch_size=self.batch_size, num_workers=2, pin_memory=True
+            self.test,
+            batch_size=self.batch_size,
+            num_workers=2,
+            pin_memory=True,
+            drop_last=True,
         )
 
 
 class Model(nn.Module):
     def __init__(self, config):
+        super().__init__()
         self.config = config
         self.num_embeddings = config.num_embeddings
         self.embedding_dim = config.embedding_dim
         self.hidden_size = config.hidden_size
-        self.seq_len = config.seq_len
+        self.sequence_length = config.sequence_length
         self.num_layers = config.num_layers
         self.bidirectional = config.bidirectional
         self.output_size = config.output_size
+        self.device = config.device
 
         self.embedding = nn.Embedding(
-            num_embeddings=self.num_embeddings, embedding_dim=self.embedding_dim
+            num_embeddings=config.num_embeddings, embedding_dim=config.embedding_dim
         )
         self.lstm = nn.LSTM(
-            input_size=self.embedding_dim,
-            hidden_size=self.hidden_size,
-            num_layers=self.num_layers,
-            bidirectional=self.bidirectional,
+            input_size=config.embedding_dim,
+            hidden_size=config.hidden_size,
+            num_layers=config.num_layers,
+            bidirectional=config.bidirectional,
             batch_first=True,
         )
-        self.linear = nn.Linear(self.hidden_size, self.output_size)
+        if config.bidirectional:
+            self.linear = nn.Linear(config.hidden_size * 2, config.output_size)
+        else:
+            self.linear = nn.Linear(config.hidden_size, config.output_size)
 
     def forward(self, x, hidden):
-        embedded = self.embedding(x)
-        lstm_out, hidden = self.lstm(embedded, hidden)
-        lstm_out = lstm_out.contiguous().view(-1, self.seq_len, 2, self.hidden_size)
+        # (batch_size, sequence_length)
+        x = x.long()
 
+        # (batch_size, sequence_length, embedding_dim)
+        embedded = self.embedding(x)
+
+        # (batch_size, sequence_length, num_directions * hidden_size)
+        lstm_out, hidden = self.lstm(embedded, hidden)
+
+        # (batch_size, sequence_length, num_directions, hidden_size)
+        lstm_out = lstm_out.contiguous().view(
+            -1, self.sequence_length, 2, self.hidden_size
+        )
+
+        # (batch_size, num_directions * hidden_size / 2)
         lstm_out_backward = lstm_out[:, 0, 1, :]
+
+        # (batch_size, num_directions * hidden_size / 2)
         lstm_out_forward = lstm_out[:, -1, 0, :]
 
         lstm_out = torch.cat((lstm_out_backward, lstm_out_forward), dim=1)
-        lstm_out = lstm_out.contiguous().view(-1, self.hidden_size)
+        # lstm_out = lstm_out.contiguous().view(-1, self.hidden_size)
 
         out = torch.relu(self.linear(lstm_out))
         out = torch.sigmoid(out)
@@ -144,24 +158,33 @@ class Model(nn.Module):
     def init_hidden(self, batch_size):
         weight = next(self.parameters()).data
         hidden = (
-            weight.new(self.num_layers * 2, batch_size, self.hidden_size).zero_(),
-            weight.new(self.num_layers * 2, batch_size, self.hidden_size).zero_(),
+            weight.new(self.num_layers * 2, batch_size, self.hidden_size)
+            .zero_()
+            .to(self.device),
+            weight.new(self.num_layers * 2, batch_size, self.hidden_size)
+            .zero_()
+            .to(self.device),
         )
         return hidden
 
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 # Define configuration
-config = {
-    "num_embeddings:": 10000,
-    "embedding_dim": 10,
-    "hidden_size": 64,
-    "seq_len": 512,
-    "num_layers": 1,
-    "bidirectional": True,
-    "output_size": 1,
-    "batch_size": 64,
-}
-config = dict_to_obj(config)
+class Config:
+    num_embeddings = 10000
+    embedding_dim = 16
+    hidden_size = 64
+    sequence_length = 512
+    num_layers = 1
+    bidirectional = True
+    output_size = 1
+    batch_size = 64
+    num_dataloader_workers = 2
+    device = device
+
+
+config = Config()
 
 # Create data module and prepare data
 data_module = DataModule(config)
@@ -174,7 +197,7 @@ test_loader = data_module.test_dataloader()
 
 # Create model
 model = Model(config)
-model.cuda()
+model.to(device)
 
 # Create optimizer
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
@@ -184,8 +207,7 @@ criterion = nn.BCELoss()
 
 # Define other training parameters
 epochs = 10
-print_interval = 1000
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print_interval = 100
 
 model.train()
 for epoch in range(epochs):
@@ -194,7 +216,8 @@ for epoch in range(epochs):
     train_accs = []
     for batch_idx, batch in enumerate(train_loader):
         x, y = batch
-        x, y = x.cuda(), y.cuda()
+        y = y.unsqueeze(1)
+        x, y = x.to(device), y.to(device)
         h = tuple([i.data for i in h])
 
         for p in model.parameters():
@@ -218,7 +241,8 @@ for epoch in range(epochs):
                 val_accs = []
                 for batch in val_loader:
                     x, y = batch
-                    x, y = x.cuda(), y.cuda()
+                    y = y.unsqueeze(1)
+                    x, y = x.to(device), y.to(device)
                     val_h = tuple([i.data for i in val_h])
 
                     output, val_h = model(x, val_h)
@@ -233,12 +257,11 @@ for epoch in range(epochs):
             print(
                 "Epoch: {}/{}".format(epoch, epochs),
                 "Step: {}".format(batch_idx),
-                "Train loss: {}".format(np.mean(train_losses.item())),
-                "Train acc: {}".format(np.mean(train_accs.item())),
-                "Val loss: {}".format(np.mean(val_losses.item())),
-                "Val acc: {}".format(np.mean(val_accs.item())),
+                "Train loss: {}".format(np.mean(train_losses)),
+                "Train acc: {}".format(np.mean(train_accs)),
+                "Val loss: {}".format(np.mean(val_losses)),
+                "Val acc: {}".format(np.mean(val_accs)),
             )
-
 
 test_losses = []
 test_accs = []
@@ -248,7 +271,7 @@ model.eval()
 with torch.no_grad():
     for batch in test_loader:
         x, y = batch
-        x, y = x.cuda(), y.cuda()
+        x, y = x.to(device), y.to(device)
         test_h = tuple([i.data for i in test_h])
 
         output, test_h = model(x, test_h)
